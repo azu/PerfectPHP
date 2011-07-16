44f393d7d457fb0e500e5ddde0e7b83966a30864
azu <azu@MacbookPro.local>
2011-07-16 22:35:19 +0900

    first commit

---
+++ Chapter1/2-1-6.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: azu
+ * Date: 11/07/16
+ * Time: 16:49
+ */
+$a = 1;
+$b = "foo bar ";
+$c = array(1, 2, 3);
+var_dump($a);
+
+var_dump($c);
+
---
+++ Chapter1/hellow.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: azu
+ * Date: 11/07/16
+ * Time: 16:08
+ * To change this template use File | Settings | File Templates.
+ */
+$number = rand();// 乱数を取得
+if($number % 2 == 0){
+    echo $number,"は偶数です", PHP_EOL;// PHPブロックの終了
+}else{
+    echo $number,"は奇数です", PHP_EOL;
+}
+?>
+PHPは終わりの始まり
+ 
